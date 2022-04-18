7060951cba7356ccc0ce978b0ee5aac593a2be62
Ziya Alim <ziyabay135@gmail.com>
2022-04-18 15:05:09 +0300

    new_update

---
+++ electron_plot.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+def draw_graf(x, y):
+    plt.plot(x, y)
+    plt.show()
+    
+def possibility():
+    e = 2.12
+    
+    
+    
+    
---
+++ learn_python_using_math/test.py
@@ -0,0 +1,7 @@
+epsilon = 0.014
+sigma = 3.4*10**-106
+a = 4*10**-10
+
+result = 4*epsilon*((156*epsilon**12)/a**14 - (42*epsilon**6)/a**8)
+
+print(result)
